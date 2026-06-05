eded9cf65e560d248e591e5e134766140e78c12e
Subham <shubhamsingh74888@gmail.com>
2026-06-06 01:33:40 +0530

    update

--- remote-backend/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-s3-bucket-wanderlust-infra-default" # Use the actual name created in s3.tf
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "wanderlust-shubham-default"   # Matches the name in dynamodb.tf
-    encrypt        = true
-  }
-}
